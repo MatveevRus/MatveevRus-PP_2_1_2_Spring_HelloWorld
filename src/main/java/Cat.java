7382013b1b57dad1dbfeb11b68919ea70f23095e
MatveevRuslan <matveevrus01@gmail.com>
2025-09-06 17:02:40 +0300

    chore: add .gitignore and remove .idea, target, and iml files

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    private String message;
+
+
+    public Cat() {
+        //  this constructor is empty
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
